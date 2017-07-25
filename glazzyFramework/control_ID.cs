8459d996fee914720f4ee5e2d593839bb93b4b79
Saydinyan <stan.aydinyan@gmail.com>
2017-07-24 17:48:26 -0700

    Update control_ID.cs

--- glazzyFramework/control_ID.cs
+++ glazzyFramework/control_ID.cs
@@ -11,14 +11,23 @@ namespace glazzyFramework
 	
 	
 	        //Update your Profile Control ID
+	//Enter UserName
         public static string username = "UserName";
+	//Enter FirstName
         public static string firstname = "FirstName";
+	//Enter LastName
         public static string lastname = "LastName";
+	//Select Date
         public static string dob = "datetimepicker1";
+	//Enter Address
         public static string address = "Address";
+	//Select State
         public static string state = "StateId";
+	//Select City
         public static string city = "CityId";
+	//Enter Zip
         public static string zip = "Pincode";
+	//Etner Phone Number
         public static string phone = "PhoneNumber";
 	
 	
